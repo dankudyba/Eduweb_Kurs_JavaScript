63275e9c7bf00042f3826a586ee6467413b592e2
Daniel Kudyba <danielkudyba@MacBook-Pro-Daniel.local>
2018-03-19 22:04:47 +0100

    Add changes to project_2

--- project_2/js/script.js
+++ project_2/js/script.js
@@ -1,36 +1,6 @@
-(function() {
+(function(){
 
-    let btn = document.querySelector("#getNumbers"),
-        output = document.querySelector("#showNumbers");
+    
 
-
-    function getRandom(min, max) {
-
-        return Math.round(Math.random()*(49-1)+1);
-
-    }
-
-    function showRandomNumbers() {
-
-        var numbers = [],
-            random;
-
-        for(var i=0; i<6; i++) {
-            random = getRandom(1, 49);
-
-            while(numbers.indexOf(random) !== -1) {
-                random = getRandom(1, 49);
-            }
-
-            numbers.push(random);
-            
-        }
-        
-        output.value = numbers.join(", ");
-    }
-
-
-    btn.onclick = showRandomNumbers;
 
 })();
-
